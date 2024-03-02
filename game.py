dbd081adedbe75f13315f19082497b38a44d4407
rlmowrey <80306835+rlmowrey@users.noreply.github.com>
2024-03-02 09:05:44 -0800

    Add files via upload

---
+++ game.py
@@ -0,0 +1,44 @@
+import pygame
+
+# Pygame setup
+pygame.init()
+screen = pygame.display.set_mode((800,500))
+pygame.display.set_caption('Codemy.com Pygame Tutorial')
+clock = pygame.time.Clock()
+running = True
+
+dt = 0 			# delta time variable
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)	# player position variable
+
+while running:
+	# poll for events
+	# pygame.QUIT event means that the user clicked the 'X' to close the window
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+
+	# Pick the screen color
+	screen.fill('silver')
+
+	# Render the game here
+	pygame.draw.circle(screen, '#033660', player_pos, 40)
+
+	# Move the circle
+	keys = pygame.key.get_pressed()
+	if keys[pygame.K_UP]:
+		player_pos.y -= 300 * dt
+	if keys[pygame.K_DOWN]:
+		player_pos.y += 300 * dt
+	if keys[pygame.K_LEFT]:
+		player_pos.x -= 300 * dt
+	if keys[pygame.K_RIGHT]:
+		player_pos.x += 300 * dt
+
+	# Flip the display to output work to the screen
+	pygame.display.flip()
+
+	# Set the clock stuff / delta time in seconds since the last frame
+	# Used for framerate-independent physics
+	dt = clock.tick(60) / 1000
+
+pygame.quit()
